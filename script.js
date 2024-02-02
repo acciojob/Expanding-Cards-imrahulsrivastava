3d8ef6bf5ef7768723efa2f2302b618ee266db43
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-02-02 12:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let prevActive = document.getElementById("panel-1");
+
+document.querySelectorAll(".panel").forEach((e) => {
+  e.addEventListener("click", (e) => {
+    prevActive.classList.remove("active");
+    e.target.classList.add("active");
+    prevActive = e.target;
+  });
+});
